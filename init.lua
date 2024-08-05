751e7f1433d60460230dec280170a099d0b0c372
Josef Schulz <j.schulz@alaun.de>
2024-08-05 11:18:46 +0200

    update

--- init.lua
+++ init.lua
@@ -341,9 +341,18 @@ require("lazy").setup({
 					},
 				}
 
+				vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+				vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+				vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+
 				vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
-				vim.fn.sign_define('DapBreakpoint', { text = '⬤', texthl = '', linehl = '', numhl = '' })
-				vim.fn.sign_define('DapStopped', { text = '->', texthl = '', linehl = '', numhl = '' })
+				vim.fn.sign_define('DapBreakpoint', { text = '⬤', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+				--vim.fn.sign_define('DapStopped', { text = '->', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+
+				vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+				vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+				vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+
 				dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 				dap.listeners.before.event_terminated['dapui_config'] = dapui.close
 				dap.listeners.before.event_exited['dapui_config'] = dapui.close

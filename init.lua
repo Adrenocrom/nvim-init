fcb6d12dbaa55a8482b2fb42a651285d070bf27a
Josef Schulz <j.schulz@alaun.de>
2024-08-06 11:19:15 +0200

    change debug view

--- init.lua
+++ init.lua
@@ -344,17 +344,77 @@ require("lazy").setup({
 					dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
 				end, { desc = 'Debug: Set Breakpoint' })
 
-				dapui.setup {
-					icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+				dapui.setup({
 					controls = {
+						element = "repl",
+						enabled = true,
 						icons = {
 							pause = '⏸',
 							play = '▶',
 							terminate = '⏹',
 							disconnect = '⏏',
-						},
+							run_last = "",
+							step_back = "",
+							step_into = "",
+							step_out = "",
+							step_over = "",
+						}
 					},
-				}
+					element_mappings = {},
+					expand_lines = true,
+					floating = {
+						border = "single",
+						mappings = {
+							close = { "q", "<Esc>" }
+						}
+					},
+					force_buffers = true,
+					icons = {
+						collapsed = "",
+						current_frame = "",
+						expanded = "",
+						--expanded = '▾', collapsed = '▸', current_frame = '*'
+					},
+					layouts = { {
+							elements = { {
+									id = "scopes",
+									size = 0.25
+								}, {
+									id = "breakpoints",
+									size = 0.25
+								}, {
+									id = "stacks",
+									size = 0.25
+								}, {
+									id = "watches",
+									size = 0.25
+								} },
+							position = "right",
+							size = 40
+						}, {
+							elements = { {
+									id = "console",
+									size = 0.8
+								}, {
+									id = "repl",
+									size = 0.2
+								} },
+							position = "bottom",
+							size = 2
+						} },
+					mappings = {
+						edit = "e",
+						expand = { "<CR>", "<2-LeftMouse>" },
+						open = "o",
+						remove = "d",
+						repl = "r",
+						toggle = "t"
+					},
+					render = {
+						indent = 1,
+						max_value_lines = 100
+					}
+				})
 
 				vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#ff0000' })
 				vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef' })

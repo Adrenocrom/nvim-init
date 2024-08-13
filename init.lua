56cf1afe0f6feb7c37a89862d2b753317f5a39f7
Josef Schulz <j.schulz@alaun.de>
2024-08-13 10:19:43 +0200

    add diagnostic symbols

--- init.lua
+++ init.lua
@@ -162,6 +162,11 @@ require("lazy").setup({
 				-- { 'folke/neodev.nvim', opts = {} },
 			},
 			config = function()
+				vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+				vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+				vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+				vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+
 				vim.api.nvim_create_autocmd('LspAttach', {
 
 					group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),

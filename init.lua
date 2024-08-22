f11e334a87a12ef49151dc50c67fec218bdd4e9c
Josef Schulz <j.schulz@alaun.de>
2024-08-23 00:28:54 +0200

    test it and may throw it away again

--- init.lua
+++ init.lua
@@ -34,6 +34,10 @@ vim.api.nvim_set_keymap('', '<Down>', '<Nop>', { noremap = true, silent = true }
 vim.api.nvim_set_keymap('', '<Left>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('', '<Right>', '<Nop>', { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', { noremap = true, silent = true })
 
 local function insertFullPath()
   local filepath = vim.fn.expand('%')
@@ -63,20 +67,51 @@ require("lazy").setup({
 		"mattn/emmet-vim",
 		"tpope/vim-surround", {
 			"nvim-telescope/telescope.nvim",
+			event = 'VimEnter',
 			version = "*",
-			dependencies = { "nvim-lua/plenary.nvim" },
+			dependencies = { 
+				'nvim-lua/plenary.nvim',
+				{					'nvim-telescope/telescope-fzf-native.nvim',
+					build = 'make',
+
+					cond = function()
+						return vim.fn.executable 'make' == 1
+					end,
+				},
+				{ 'nvim-telescope/telescope-ui-select.nvim' },
+			},
 			config = function()
+				require('telescope').setup {
+					extensions = {
+						require("telescope.themes").get_dropdown()
+					}
+				}
+
+				pcall(require('telescope').load_extension, 'fzf')
+				pcall(require('telescope').load_extension, 'ui-select')
+
 				local builtin = require('telescope.builtin')
 				vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 				vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+				vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+				vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+				vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+				vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+				vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+				vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+				vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+				vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+				vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+				vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 			end
-		}, {
+		}, --[[{
 			'nvim-telescope/telescope-fzf-native.nvim',
 			build = 'make',
 			cond = function()
 				return vim.fn.executable 'make' == 1
 			end
-		}, {
+		},]]-- 
+		{
 			"tpope/vim-fugitive",
 			config = function()
 				vim.api.nvim_set_keymap("n", "<leader>gb", ":Git blame<CR>", {})
@@ -99,7 +134,7 @@ require("lazy").setup({
 					}
 				})
 
-				vim.cmd.NvimTreeOpen()
+				--vim.cmd.NvimTreeOpen()
 			end
 		},
 		{

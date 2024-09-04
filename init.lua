90bb7c0f630ed57d9defd8b2a7915465c673a1b4
Josef Schulz <j.schulz@alaun.de>
2024-09-04 10:15:25 +0200

    update

--- init.lua
+++ init.lua
@@ -263,7 +263,7 @@ require("lazy").setup({
 								'-jar',
 								'/home/josef/Dokumente/alaun/org.alaun.cauldron.ls/target/ls-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
 							},
-							root_dir = lspconfig.util.root_pattern("*.cld", "pom.xml"),
+							root_dir = lspconfig.util.root_pattern("*.cld", "pkg.module.cld", "pom.xml", ".cldroot"),
 							filetypes = { 'xml.cauldron' },
 						},
 				  	}

bc4f78a164637c0c22d0f7c8dd4c127c6bb0b5a1
Josef Schulz <j.schulz@alaun.de>
2025-05-14 12:58:20 +0200

    update

--- init.lua
+++ init.lua
@@ -218,10 +218,16 @@ require("lazy").setup({
 			},
 
 			config = function()
-				vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-				vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-				vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-				vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+				vim.diagnostic.config({
+					signs = {
+						text = {
+							[vim.diagnostic.severity.ERROR] = '',
+							[vim.diagnostic.severity.WARN] = '',
+							[vim.diagnostic.severity.INFO] = '',
+							[vim.diagnostic.severity.HINT] = '',
+						}
+					}
+				})
 
 				vim.api.nvim_create_autocmd('LspAttach', {
 

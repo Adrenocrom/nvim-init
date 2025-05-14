881c794eabf1f532529a9df138f7ae99c826d365
Josef Schulz <j.schulz@alaun.de>
2025-05-14 10:33:43 +0200

    doe

--- init.lua
+++ init.lua
@@ -253,6 +253,13 @@ require("lazy").setup({
 						vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls")
 					},
 					root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.pom', '.git', 'mvnw'}, { upward = true })[1]),
+					init_options = {
+						bundles = {
+							vim.fn.glob(
+								vim.fn.expand(vim.fn.expand("~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.53.1.jar"), 1)
+							)
+						}
+					},
 				})
 			end
 		},

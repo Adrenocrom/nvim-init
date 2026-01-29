4720f7fd469012135fb189b8d96502fb76ffc496
Josef Schulz <j.schulz@alaun.de>
2026-01-29 09:59:26 +0100

    switch from gen.nvim to opencode.nvim

--- init.lua
+++ init.lua
@@ -195,33 +195,44 @@ require("lazy").setup({
 			end
 		},
 		{
-			"David-Kunz/gen.nvim",
-			config = function ()
-				local model_name = "gpt-oss:20b";
-				require('gen').setup({
-					model = model_name,
-					display_mode = "vertical-split",
-					show_prompt = true,
-					show_model = true,
-					no_auto_close = true
-				})
-
-				require('gen').prompts['Fix_Code'] = {
-					prompt = "Fix the following code. Only output the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
-					replace = true,
-					extract = "```$filetype\n(.-)```"
+			"NickvanDyke/opencode.nvim",
+			dependencies = {
+				-- Recommended for `ask()` and `select()`.
+				-- Required for `snacks` provider.
+				---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+				{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+			},
+			config = function()
+				---@type opencode.Opts
+				vim.g.opencode_opts = {
+					-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+					provider = {
+						snacks = {
+							win = {
+								position = "left"
+							}
+						}
+					}
 				}
 
-				require('gen').prompts['Shorter_Code'] = {
-					prompt = "Rewrite the code as short as possible. Only output the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
-					replace = true,
-					extract = "```$filetype\n(.-)```"
-				}
+				-- Required for `opts.events.reload`.
+				vim.o.autoread = true
 
-				vim.keymap.set('n', '<leader>a', vim.cmd.Gen, { desc = ' Ai agent' })
-				vim.keymap.set('v', '<leader>a', ":'<,'>Gen<cr>", { desc = ' Ai agent' })
-				vim.keymap.set('n', '<leader>cm', require('gen').select_model, { desc = ' Ai change model' })
-			end
+				-- Recommended/example keymaps.
+				vim.keymap.set({ "n", "x" }, "<leader>a", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = " Ai Ask opencode…" })
+				vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+				vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+
+				vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+				vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+
+				vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+				vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+
+				-- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o…".
+				vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+				vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+			end,
 		},
 		{
 			"milanglacier/minuet-ai.nvim",
